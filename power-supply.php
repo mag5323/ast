fb675bd625effacead5ea4ea14d83e420ccd0916
mag5323 <mag5323@gmail.com>
2017-09-15 20:47:25 +0800

    Adjust power-supply layout

--- power-supply.php
+++ power-supply.php
@@ -14,16 +14,25 @@
 ?>
   <DIV class="featured clearfix">
       <H2 class="ac">製品紹介<br><small>パワーサプライ</small></H2>
-    <div class="col-sm-4 col-sm-offset-4">
+    <div class="col-sm-4 col-sm-offset-2">
         <div class="well">
             <img src="files/power-supply/IP65.png" class="center-block" height="75">
+            <h5 class="text-center">調節可能　100W / 120W / 150W / 185W</h5>
         </div>
         <div class="list-group">
             <a href="files/power-supply/Datasheet_lne-12v100w調光.pdf" class="list-group-item" target="_blank">
-                <span class="glyphicon glyphicon-chevron-right"></span>&nbsp IR Renaissance 130º
+                <span class="glyphicon glyphicon-chevron-right"></span>12V　100V　データシート
             </a>
+        </div>
+    </div>
+    <div class="col-sm-4">
+        <div class="well">
+            <img src="files/power-supply/IP67.png" class="center-block" height="75">
+            <h5 class="text-center">調光可能　100W / 120W / 150W / 185W</h5>
+        </div>
+        <div class="list-group">
             <a href="files/power-supply/Datasheet_lne-12v100w調節可能.pdf" class="list-group-item" target="_blank">
-                <span class="glyphicon glyphicon-chevron-right"></span>&nbsp IR Renaissance 140º
+                <span class="glyphicon glyphicon-chevron-right"></span>12V　100V　データシート
             </a>
         </div>
     </div>
