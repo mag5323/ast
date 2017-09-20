7fc119e8a7cb0fd6fba0ddefa80908a25cfe45b0
mag5323 <mag5323@gmail.com>
2017-09-20 21:55:31 +0800

    Add Led-lamps images

--- LED-lamps.php
+++ LED-lamps.php
@@ -16,12 +16,18 @@
   <DIV class="featured clearfix">
       <H2 class="ac">製品紹介<br><small>LED Lamps</small></H2>
     <div class="col-sm-3">
+        <div class="well">
+            <a href="files/LED-lamps/3mm.png" target="_blank"> <img src="files/LED-lamps/3mm.png" class="center-block" height="125" width="248"> </a>
+        </div>
         <div class="list-group">
             <a href="files/3mm_30.pdf" target="_blank" class="list-group-item"><span class="glyphicon glyphicon-chevron-right"></span>&nbsp 3mm 30度</a>
             <a href="files/3mm_60.pdf" target="_blank" class="list-group-item"><span class="glyphicon glyphicon-chevron-right"></span>&nbsp 3mm 60度</a>
         </div>
     </div>
     <div class="col-sm-3">
+        <div class="well">
+            <a href="files/LED-lamps/5mm.png" target="_blank"> <img src="files/LED-lamps/5mm.png" class="center-block" height="125" width="248"> </a>
+        </div>
         <div class="list-group">
             <a href="files/5mm_15.pdf" target="_blank" class="list-group-item"><span class="glyphicon glyphicon-chevron-right"></span>&nbsp 5mm 15度</a>
             <a href="files/5mm_23.pdf" target="_blank" class="list-group-item"><span class="glyphicon glyphicon-chevron-right"></span>&nbsp 5mm 23度</a>
@@ -31,11 +37,17 @@
         </div>
     </div>
     <div class="col-sm-3">
+        <div class="well">
+            <a href="files/LED-lamps/Bi_color.png" target="_blank"> <img src="files/LED-lamps/Bi_color.png" class="center-block" height="125" width="248"> </a>
+        </div>
         <div class="list-group">
             <a href="files/Bi_color.pdf" target="_blank" class="list-group-item"><span class="glyphicon glyphicon-chevron-right"></span>&nbsp Bi color</a>
         </div>
     </div>
     <div class="col-sm-3">
+        <div class="well">
+            <a href="files/LED-lamps/oval.png" target="_blank"> <img src="files/LED-lamps/oval.png" class="center-block" height="125" width="248"> </a>
+        </div>
         <div class="list-group">
             <a href="files/oval_2K.pdf" target="_blank" class="list-group-item"><span class="glyphicon glyphicon-chevron-right"></span>&nbsp oval 2K</a>
             <a href="files/oval_5K.pdf" target="_blank" class="list-group-item"><span class="glyphicon glyphicon-chevron-right"></span>&nbsp oval 5K</a>
