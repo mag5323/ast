9a22699e63a95a615aa29464e5ee3736a9cb6780
mag5323 <mag5323@gmail.com>
2017-10-18 20:18:19 +0800

    Add LED-light img

--- LED-light.php
+++ LED-light.php
@@ -14,10 +14,24 @@
 
   <DIV class="featured clearfix">
       <H2 class="ac">製品紹介<br><small>LED 照明</small></H2>
-    <div class="col-sm-4 col-sm-offset-4">
+    <div class="col-sm-4 col-sm-offset-2">
+        <div class="well">
+            <img src="files/LED-light/PAR38.png" class="center-block" height="75">
+        </div>
         <div class="list-group">
-            <a href="files/LED-light/PAR38X2.pdf" target="_blank" class="list-group-item"><span class="glyphicon glyphicon-chevron-right"></span>&nbsp PAR38X2</a>
-            <a href="files/LED-light/PAR52U.pdf" target="_blank" class="list-group-item"><span class="glyphicon glyphicon-chevron-right"></span>&nbsp PAR52U-B2</a>
+            <a href="files/LED-light/PAR38X2.pdf" class="list-group-item" target="_blank">
+                <span class="glyphicon glyphicon-chevron-right"></span>>&nbsp PAR38X2
+            </a>
+        </div>
+    </div>
+    <div class="col-sm-4">
+        <div class="well">
+            <img src="files/LED-light/PAR52U-B2.png" class="center-block" height="75">
+        </div>
+        <div class="list-group">
+            <a href="files/LED-light/PAR52U.pdf" class="list-group-item" target="_blank">
+                <span class="glyphicon glyphicon-chevron-right"></span>>&nbsp PAR52U-B2
+            </a>
         </div>
     </div>
   </DIV>
