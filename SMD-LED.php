a5dddb78da12ab502605a690cc2e8121855712d3
mag5323 <mag5323@gmail.com>
2017-10-18 21:26:56 +0800

    Add SMD img

--- SMD-LED.php
+++ SMD-LED.php
@@ -15,26 +15,42 @@
 
   <DIV class="featured clearfix">
       <H2 class="ac">製品紹介<br><small>SMD LED</small></H2>
-    <div class="col-sm-3">
-        <div class="list-group">
-            <a href="files/SMD/3528_P2.pdf" target="_blank" class="list-group-item"><span class="glyphicon glyphicon-chevron-right"></span>&nbsp 3528 P2</a>
-            <a href="files/SMD/3528_P4.pdf" target="_blank" class="list-group-item"><span class="glyphicon glyphicon-chevron-right"></span>&nbsp 3528 P4</a>
+    <div class="row">
+        <div class="col-sm-6">
+            <div class="col-sm-8 col-sm-offset-2">
+                <div class="well">
+                    <img src="files/SMD/SMD-L.png" class="center-block" style="width:137px">
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="list-group">
+                    <a href="files/SMD/3528_P2.pdf" target="_blank" class="list-group-item"><span class="glyphicon glyphicon-chevron-right"></span>&nbsp 3528 P2</a>
+                    <a href="files/SMD/3528_P4.pdf" target="_blank" class="list-group-item"><span class="glyphicon glyphicon-chevron-right"></span>&nbsp 3528 P4</a>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="list-group">
+                    <a href="files/SMD/2835.pdf" target="_blank" class="list-group-item"><span class="glyphicon glyphicon-chevron-right"></span>&nbsp 2835</a>
+                    <a href="files/SMD/5050.pdf" target="_blank" class="list-group-item"><span class="glyphicon glyphicon-chevron-right"></span>&nbsp 5050</a>
+                </div>
+            </div>
         </div>
-    </div>
-    <div class="col-sm-3">
-        <div class="list-group">
-            <a href="files/SMD/2835.pdf" target="_blank" class="list-group-item"><span class="glyphicon glyphicon-chevron-right"></span>&nbsp 2835</a>
-            <a href="files/SMD/5050.pdf" target="_blank" class="list-group-item"><span class="glyphicon glyphicon-chevron-right"></span>&nbsp 5050</a>
-        </div>
-    </div>
-    <div class="col-sm-3">
-        <div class="list-group">
-            <a href="files/SMD/Bi_color.pdf" target="_blank" class="list-group-item"><span class="glyphicon glyphicon-chevron-right"></span>&nbsp Bi color</a>
-        </div>
-    </div>
-    <div class="col-sm-3">
-        <div class="list-group">
+        <div class="col-sm-6">
+            <div class="col-sm-8 col-sm-offset-2">
+                <div class="well">
+                    <img src="files/SMD/SMD-R.png" class="center-block" style="width:106px">
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="list-group">
+                    <a href="files/SMD/Bi_color.pdf" target="_blank" class="list-group-item"><span class="glyphicon glyphicon-chevron-right"></span>&nbsp Bi color</a>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="list-group">
             <a href="files/SMD/multi_color.pdf" target="_blank" class="list-group-item"><span class="glyphicon glyphicon-chevron-right"></span>&nbsp Multi color</a>
+                </div>
+            </div>
         </div>
     </div>
   </DIV>
