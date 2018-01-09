0861e2770cb3cf6067c6d10226c1dfe2a4dbff57
JohnsonLu <johnson4932@gmail.com>
2018-01-09 18:14:06 +0800

    add template

---
+++ dashboard/index.php
@@ -0,0 +1,31 @@
+<?php
+    require 'include/check.php';
+?><!DOCTYPE html>
+<html>
+<?php include 'include/header.php'; ?>
+<body>
+    <div id="wrapper">
+        <!-- Navigation -->
+<?php include 'include/nav.php'; ?>
+        <div id="page-wrapper">
+            <div class="container-fluid">
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                            標題
+                        </h1>
+                    </div>
+                </div>
+                <!-- /.row -->
+
+                <div class="row">
+                    <div class="col-lg-12">
+                    </div><!-- end .col-lg-12-->
+                </div><!-- end .row -->
+            </div><!-- end .container-fluid -->
+        </div><!-- /#page-wrapper -->
+    </div><!-- /#wrapper -->
+</body>
+</html>
+
