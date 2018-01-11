99bc38d36ce116a252f87f05ced24a74a4153581
JohnsonLu <johnson4932@gmail.com>
2018-01-11 17:48:42 +0800

    use DB data

--- Profile.php
+++ Profile.php
@@ -1,4 +1,17 @@
-﻿<!DOCTYPE HTML>
+﻿<?php
+require 'connection.php';
+
+$db = $GLOBALS['db'];
+$sql = "SELECT * FROM info WHERE field_name IN ('story_info', 'story', 'technology', 'design', 'oem_service', 'oem_service2', 'about')";
+$rs = $db->query($sql);
+$raws = $rs->fetchAll();
+
+$arr = [];
+foreach ($raws as $raw) {
+    $arr[$raw['field_name']] = $raw['context'];
+}
+
+?><!DOCTYPE HTML>
 <HTML>
 <head>
 <?php
@@ -18,19 +31,10 @@
     <H2 class="ac">私たちの歴史</H2>
     <DIV class="row">
       <DIV class="col-sm-6">
-        <P>会社名:     有限会社 エー・エス・テック<br>
-          創立者:       天野 工<br>
-          代表取締役:	Jay Huang<br>
-          所在地:     大阪府東大阪市長田東3-2-43<br>
-          TEL:     06-6748-3660(代)<br>
-          FAX:     06-6747-3670<br>
-          設立:     2000年</P>
+        <?php echo $arr['story_info']; ?>
       </DIV>
       <DIV class="col-sm-6">
-        <P>1995年からLEDを中心とする電子部品の輸入販売をスタートしました。<br>
-          当初、日本メーカと同等或いはそれ以上の品質レベルを目標に、海外電子部品メーカーの製品を徹底的に研究いたしました。<br>
-          その結果、台湾LEDメーカーの一つであるSANDER ELECTRONICS(三得電子)とのタイアップを開始しました。<br>
-          品質レベルを1~3 PPMを目標に品質改善を繰り返し、ある案件で約 300万個ご使用で 1個の NG という品質レベルが達成できたのを契機に、有限会社エー・エス・テックを設立いたしました。</P>
+        <?php echo $arr['story']; ?>
       </DIV>
     </DIV>
   </DIV>
@@ -41,8 +45,7 @@
       <DIV class="col-sm-6">
         <DIV class="contact-box">
           <H1 id="except">最先端の技術</H1>
-          <P>その後 LED も年々高輝度化が進み、交通信号機に LED が使用される事になり、益々LED 応用製品の多様化が図られ、弊社でも2003 年初めより高輝度 LED を使用したLED 電球を発売しました。 <br>
-            2016年より、長年のパートナーであるSANDER ELECTRONICSと合併を行い、より多くのSANDER ELECTRONICS製品を日本市場に提供すると共に、高品質な新商品の創出を目指します。</P>
+            <?php echo $arr['technology']; ?>
         </DIV>
       </DIV>
     </DIV>
@@ -51,7 +54,7 @@
       <DIV class="col-sm-6">
         <DIV class="contact-box">
           <H1>設計支援</H1>
-          <P>SANDER ELECTRONICSは高品質と優れたオーダーメイド商品製造技術を武器に、北米、南米、ヨーロッパ、中東、アフリカ、アジア、オセアニアの30か国以上でサービスを提供しています。アメリカ、ヤンキーススタジアムのLEDディスプレイや日本の大阪通天閣のLED等、SANDER ELECTRONICSの名前を聴いたことが無い方も、SANDER ELECTRONICSの放つ光を見たことはあるのではないでしょうか。</P>
+          <?php echo $arr['design']; ?>
         </DIV>
       </DIV>
       <DIV class="col-sm-6"><img src="images/SERVICE.PNG" height="380"></DIV>
@@ -62,13 +65,7 @@
       <DIV class="col-sm-6">
         <DIV class="contact-box">
           <H1> OEMサービス</H1>
-          <P> エーエステックではLEDディスプレイ等のOEMサービスを行っています。
-
-            パートナーであるSANDER ELECTRONICS共に様々な製品をお客様に提供してきました。
-
-            お客様からの厳しい品質要求に応えるため、品質管理部門と連携し、徹底した品質管理を実施しております。
-
-            豊富な経験が高度な技術力と安定した品質を保証いたします。</P>
+            <?php echo $arr['oem_service']; ?>
         </DIV>
       </DIV>
     </DIV>
@@ -77,23 +74,14 @@
       <DIV class="col-sm-6">
         <DIV class="contact-box">
           <H1> ODMサービス</H1>
-          <P> エーエステックはOEMと共にODMでお客様をサポート致します。
-
-            新規LED製品開発をお考えのお客様はご気軽にご連絡ください。
-
-            主にLEDディスプレイ、LED照明(投光器等)、PCB設計を得意とし、世界中に製品を提供しています。
-
-            SANDER ELECTRONICSとは長期に渡り各種LEDの研究を行ってきました。お客様に合ったオリジナルのLEDを作製致します。
-
-            弊社ではLED製品提案、開発、製造を一貫して行います。適切な価格で高品質の商品をお客様に提供いたします。</P>
+            <?php echo $arr['oem_service2']; ?>
         </DIV>
       </DIV>
       <DIV class="col-sm-6"> <img src="images/ASSEMBLY.png" height="380"></DIV>
     </DIV>
     <DIV class="profile-mini-banner-05">
       <H1>エー・エス・テックについて</H1>
-      <P>今後エーエステックはSANDER ELECTRONICSのサポートを受けながら商品ラインナップを増やし、OEM/ODMサービスを提供します。
-        また弊社ではSANDER ELECTRONICSのグローバルネットワークを活かし、日本の素晴らしい商品を、世界中に提供する輸出入業務も行っております。</P>
+        <?php echo $arr['about']; ?>
     </DIV>
   </DIV>
   </SECTION>
