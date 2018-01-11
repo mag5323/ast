095b32012f03fb97e1e7976ea3b504d0a43afd0d
JohnsonLu <johnson4932@gmail.com>
2018-01-11 17:57:14 +0800

    update url

--- dashboard/include/nav.php
+++ dashboard/include/nav.php
@@ -6,14 +6,36 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#">Dashboard</a>                                                                                                                                             </div>
+                <a class="navbar-brand" href="/dashboard">Dashboard</a>
+            </div>
             <ul class="nav navbar-right top-nav">
                 <li><a class="navbar-link" href="#"><i class="fa fa-user"></i> admin </a></li>
             </ul>
             <div class="collapse navbar-collapse navbar-ex1-collapse">
                 <ul class="nav navbar-nav side-nav">
                     <li>
-                        <a href="/dashboard"><i class="fa fa-fw fa-dashboard"></i> 首頁</a>
+                        <a href="/"><i class="fa fa-fw fa-dashboard"></i> WebSite</a>
+                    </li>
+                    <li>
+                        <a href="/dashboard/story_info.php"><i class="fa fa-fw fa-dashboard"></i> 私たちの歴史_info</a>
+                    </li>
+                    <li>
+                        <a href="/dashboard/story.php"><i class="fa fa-fw fa-dashboard"></i> 私たちの歴史</a>
+                    </li>
+                    <li>
+                        <a href="/dashboard/technology.php"><i class="fa fa-fw fa-dashboard"></i> 最先端の技術</a>
+                    </li>
+                    <li>
+                        <a href="/dashboard/design.php"><i class="fa fa-fw fa-dashboard"></i> 設計支援</a>
+                    </li>
+                    <li>
+                        <a href="/dashboard/oem_service.php"><i class="fa fa-fw fa-dashboard"></i> OEMサービス</a>
+                    </li>
+                    <li>
+                        <a href="/dashboard/oem_service2.php"><i class="fa fa-fw fa-dashboard"></i> ODMサービス2</a>
+                    </li>
+                    <li>
+                        <a href="/dashboard/about.php"><i class="fa fa-fw fa-dashboard"></i> エー・エス・テックについて</a>
                     </li>
                     <li>
                         <a href="/dashboard/logout.php"><i class="fa fa-fw fa-sign-out"></i> 登出</a>
